104003b04b21eed8d7d64cef35a560a5864afaa7
Adel <prop.het@mail.ru>
2021-06-26 19:45:38 +0300

    Добавлена бд

---
+++ Course-Project.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE Parking
+
+USE Parking
+go
+
+CREATE TABLE Admin(
+	id int identity(1,1),
+	name nchar(15) not null,
+	surname nchar(15) not null,
+	pass nchar(18) not null,
+	CONSTRAINT Prim_ID_Admin PRIMARY KEY (id),
+)  
+GO
+
+CREATE TABLE Place(
+	id int identity(1,1),
+	status nchar(9) not null,
+	CONSTRAINT Prim_ID_Place PRIMARY KEY (id),
+)
+GO
+
+CREATE TABLE Report(
+	id int identity(1,1),
+	car nchar(10) not null,
+	place int not null,
+	arrival smalldatetime not null,
+	CONSTRAINT Prim_ID_Report PRIMARY KEY (id),
+	CONSTRAINT Fore_Place_Report FOREIGN KEY (place) REFERENCES Place([id])
+)
+GO
+
+CREATE TABLE Payment(
+	id int not null,
+	departure smalldatetime not null,
+	pay smallmoney not null,
+	CONSTRAINT Fore_ID_Report FOREIGN KEY (id) REFERENCES Report([id])
+)
+GO
