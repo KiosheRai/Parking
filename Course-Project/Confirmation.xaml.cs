8fd216ae340a341651e535280aaf3f0c1afd51bc
Adler <prop.het@mail.ru>
2021-06-28 21:56:44 +0300

    Окно входа готово

---
+++ Course-Project/Confirmation.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Course_Project
+{
+    /// <summary>
+    /// Логика взаимодействия для Confirmation.xaml
+    /// </summary>
+    public partial class Confirmation : Page
+    {
+
+        public Confirmation(string num, string s, string time)
+        {
+            InitializeComponent();
+
+
+            StartCloseTimer(5);
+            ShowOnTime(num, s, time);
+        }
+
+        private void StartCloseTimer(int s)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(s);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            DispatcherTimer timer = (DispatcherTimer)sender;
+            timer.Stop();
+            timer.Tick -= TimerTick;
+            NavigationService.Navigate(new Uri("/Entrance.xaml", UriKind.Relative));
+        }
+
+
+        private void ShowOnTime(string num, string s, string time)
+        {
+            NumberOutput.Text = num;
+            PlaceOutput.Text = s;
+            TimeOutput.Text = time;
+        }
+    }
+}
--- Course-Project/Entrance.xaml.cs
+++ Course-Project/Entrance.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -24,7 +25,36 @@ namespace Course_Project
         public Entrance()
         {
             InitializeComponent();
-            ComboBoxView.ItemsSource = SQLbase.Select($"select * from Place where status = 'Свободно'").DefaultView;
+
+            CheckFreePlace();
+        }
+
+        private void CheckFreePlace()// Проверка присутствия свободных мест
+        {
+            DataTable table = SQLbase.Select($"select * from Place where status = 'Свободно'");
+
+            ComboBoxView.ItemsSource = table.DefaultView;
+
+            if (table.Rows.Count == 0)
+            {
+                //IEnumerable<Button> collection = grid1.Children.OfType<Button>();
+                //foreach(Button x in collection)
+                //{
+                //    if(x.Content.ToString() == "Продолжить")
+                //    {
+                //        x.IsVisible = false;
+                //    }
+                //}
+                ButtonContinue.Visibility = Visibility.Hidden;
+                NumberInput.Foreground = Brushes.Red;
+                NumberInput.IsReadOnly = true;
+                NumberInput.Text = "Мест нет!";
+                return;
+            }
+
+            NumberInput.IsReadOnly = false;
+            ButtonContinue.Visibility = Visibility.Visible;
+            NumberInput.Foreground = Brushes.Black;
         }
 
         private void Resume(object sender, RoutedEventArgs e)
@@ -38,11 +68,63 @@ namespace Course_Project
             {
                 MessageBox.Show("Гос номера не обслуживаем!");
                 Application.Current.Shutdown();
+                return;
             }
 
+            //Проверка правильности ввода значений
             if (!b)
             {
-                
+                NumberInput.Foreground = Brushes.Red;
+                NumberInput.ToolTip = "Неверный формат";
+            }
+            else
+            {
+                NumberInput.Foreground = Brushes.Black;
+                NumberInput.ToolTip = "";
+            }
+
+            //if(ComboBoxView.SelectedIndex == -1)
+            //{
+            //    ComboBoxView.Foreground = Brushes.Red;
+            //    ComboBoxView.ToolTip = "Выберите место";
+            //}
+            //else
+            //{
+            //    ComboBoxView.Foreground = Brushes.Black;
+            //    ComboBoxView.ToolTip = "";
+            //}
+
+            //Занесение значений в бд
+            if (b && ComboBoxView.SelectedIndex != -1)
+            {
+                DataTable tableReport = SQLbase.Select($"select * from Report where car = '{num}'");
+
+                if(tableReport.Rows.Count >= 1)
+                {
+                    NumberInput.Foreground = Brushes.Red;
+                    NumberInput.ToolTip = "Машина уже присутствует.";
+                    return;
+                }
+                else
+                {
+                    NumberInput.Foreground = Brushes.Black;
+                    NumberInput.ToolTip = "";
+                }
+
+                int index = ComboBoxView.SelectedIndex + 1;
+                string s = ComboBoxView.Text;
+
+                string time = DateTime.Now.ToString();
+
+                SQLbase.Insert($"INSERT INTO Report(car, place, arrival) values('{num}', {s}, '{time}');");
+                SQLbase.Insert($"UPDATE Place SET status = 'Занято' where id = {s}");
+
+                //Переход на другую страницу с параметром
+                NavigationService nav;
+                nav = NavigationService.GetNavigationService(this);
+
+                Confirmation nextPage = new Confirmation(num, s, time);
+                nav.Navigate(nextPage);
             }
         }
     }
